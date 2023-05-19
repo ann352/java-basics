c7a1a15f6542af00aebe8a51d080611edbfd54e8
ann352 <anna.woznickaaa@gmail.com>
2023-05-19 19:47:18 +0200

    FX-01 added information about loops

--- src/src/Main.java
+++ src/src/Main.java
@@ -52,10 +52,87 @@ public class Main {
         Scanner scanner2 = new Scanner(System.in);
         int age = scanner2.nextInt();
 
-        if ( age >= 18) {
+        if (age >= 18) {
             System.out.println("You are adult. You can buy alkohol!");
-        }
-        else if (age < 18) {
+        } else if (age < 18) {
             System.out.println("You can't buy alkohol! You are too young!");
         }
-    }}
+
+        // 2. PRIMITIVE TYPES - SPECIFIES VALUE AND TYPE OF VARIABLE VALUES AND IT HAS NO ADDITIONAL METHODS Like reference type String (e.g. endsWith())
+        // java it is static typed language. so when we create specific variable with given type -> e.g. string we can't put to this variable other type
+        // from java10 -> we have type 'var'  - this type guess what is the value type of variable
+        // additionally in java we have about 50 reserved words and we cannot use those words to name variables
+
+        // 3. BOOLEAN - we use this type to check if expression evaluate to true or false -> value of those expression can be type boolean ->
+        // == -> equal ; > ; < ; !=
+
+        boolean isSkyBlue = true;
+
+        if (isSkyBlue) {
+            System.out.println("Sky is blue today");
+        } else {
+            System.out.println("Sky isn't blue today!");
+        }
+
+        // 4. LOOPS in Java  ->  a) FOR  ,  b) WHILE -> the main action that loop should do is to repeat given action specified number of times
+
+        // FOR
+
+        // when we create for loop we should specified (we specified initial codition ; ending condition ; and what should be done every time -> incremetation or decrementation)
+        // in curly brackets we write what loop should do in every step
+
+        for (int i = 1, j = 10; i <= 9; i += 2, j++) {
+            System.out.println(i);
+            System.out.println(j);
+            System.out.println();
+        }
+
+
+        // WHILE
+
+        // until condition is true -> loop is supposed to run
+
+        int k = 1;
+
+        while (k < 10) {
+            System.out.println("k is smaller then 10");
+            k++;
+        }
+        // variation of while loop is DO-WHILE -> at first loop is executed and later we check condition -> so loop will be executed last once
+
+        do {
+            System.out.println("First execution of the do-while loop");
+        }
+        while (1 > 2);
+
+
+        // BREAK & CONTINUE
+
+        for (int i = 0; i <= 10; i++) {
+            if (i == 3) {
+                continue; // we come to 2. break actual executing of the loop and do next circle
+            }
+            if (i == 7) {
+                break; // we come only to 7 and break all the loop
+            }
+            System.out.println(i);
+        }
+
+
+        // THE GAME FIZZ-BUZZ
+
+        for (int i = 1; i < 101; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {  //this condition is first cause when we check if 'i' is divisible by 3, we wouldn't check later if it is divisible by 5, too
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+
+    }
+}
