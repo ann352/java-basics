3426e65bbf3edaffbf45b00a5d0fc55f247c1cd6
ann352 <anna.woznickaaa@gmail.com>
2023-05-25 12:46:04 +0200

    FX-01 added information about tables and for-each loop

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,4 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*; //here we have all methods for collections
 
 public class Main {
 
@@ -164,33 +163,130 @@ public class Main {
 
         // DATA STRUCTURES - we have those structures for easy finding and operating similar values
 
+        // TABLES
         // the first important structure in Java are tables -> e.g.  we have similar data on every site in binder
         // the size of table we specified when table is created and to extend old table we have to crate new table, and copied values from the old
 
         // the number of persons at the party
-        String [] names = new String [4];
+        String[] names = new String[4];
 
         // we put values to the table in this way -> we are starting from '0' or in that way -> String[]names = {"Tomek","Romek","Asia","Stasia" }
-        names [0] = "Tomek";
-        names [1] = "Romek";
-        names [2] = "Asia";
-        names [3] = "Basia";
+        names[0] = "Tomek";
+        names[1] = "Romek";
+        names[2] = "Asia";
+        names[3] = "Basia";
 
         //when we add new value e.g. here names[3]="Stasia" then in table we will have only the new value
         //when we try to add to table value for table index that not exist we get -> ArrayIndexOutOfBoundsException -> names [4] = "Jasia";
 
         //we can show what values we have in the table by for loop , by using method that returns the size of table
-        for(int i = 0; i < names.length ; i++){
+        for (int i = 0; i < names.length; i++) {
             System.out.println(names[i]);
         }
 
         //here is another way to show values of the table - by using 'for' loop
         // we define the type and name of single element and after ':' we indicate in which table we iterate -> later we only show every single element
 
-        for(String tableName : names){
+        for (String tableName : names) {
             System.out.println(tableName);
         }
 
+        //LISTS
+        //we don't have to declare the size of collection when we crate list, we can add or remove elements from list when we want
+        //when we add elements in order, they are placed in list in that order and when we add element to specified index, then element is available under the index
+        // we connstruate list in that way -> List<typeOfDataInList> nameOfList = new ArrayList<>(); -> and we have to import ArrayList
 
+        List<String> firstNames = new ArrayList<>();
+
+        firstNames.add(0, "Tomek");
+        firstNames.add("Błażej");
+        firstNames.add("Stanisław");
+        firstNames.add("Marcin");
+        firstNames.add("Lukasz");
+        //array list is a structure when we can have duplicates so that is no problem when we add the same element more than once
+        firstNames.add("Lukasz");
+        //now when we try to remove Lukasz we remove only the first occurance of the "Lukasz" when we want to remove 2 occurances we have tu run method 2 times
+        firstNames.remove("Lukasz");
+
+        for (String firstName :
+                firstNames) {
+            System.out.println(firstName);
+        }
+
+        firstNames.remove("Lukasz");
+        firstNames.remove(0);
+
+        for (int i = 0; i < firstNames.size(); i++) {
+            System.out.println(firstNames.get(i));
+        } //-> jak wyswietlic element listy w petli for?
+
+        //we can check if list contains given element
+        System.out.println(firstNames.contains("Błażej"));
+
+        //we can get chosen element of list by inserting its index
+        System.out.println(firstNames.get(0));
+
+        //when we want sort our list alphabetically, we can use Collections.sort(ourList);
+        Collections.sort(firstNames);
+
+        //we can check if list is empty
+        System.out.println("Is firstNames list empty? " + firstNames.isEmpty());
+
+        //we clear the list
+        firstNames.clear();
+        System.out.println(firstNames.isEmpty());
+
+        //when we do a list with food for guest, and we write what they want to eat in list we can have duplicates
+        //but when we don't want to have duplicates we can use SET
+
+        // SETS
+        // are similar like list but have two differnces: 1) we don't have duplicates here, 2) when we read the element from SET we have no guarantee about the order
+        // we have similar methods like for lists, we can also convert our set to table -> toArray();
+
+        Set<String> meals = new HashSet<>();
+        meals.add("Pizza");
+        meals.add("Burger");
+        meals.add("Pizza");
+        meals.add("Pizza");
+        meals.add("Salad");
+
+        for (String meal : meals) {
+            System.out.println(meal);
+        }
+
+        //HASH MAP (structure key-value)
+        Map<String, String> nameToMeal = new HashMap<>();
+        nameToMeal.put("Tomek", "Pizza");
+        nameToMeal.put("Błażej", "Burger");
+        nameToMeal.put("Stanislaw", "Spagetti");
+        nameToMeal.put("Lukasz", "Burger");
+
+        //when we try to add new value under existing key the old value will be replaced by the old
+        nameToMeal.put("Lukasz", "Spagetti");
+
+        //when we want to get given value from hash map, we have to use the key
+        System.out.println(nameToMeal.get("Lukasz"));
+
+        //we can also check if our map isEmpty() and we can also clear its values
+        System.out.println(nameToMeal.isEmpty());
+        // nameToMeal.clear();
+        // System.out.println(nameToMeal.isEmpty());
+        // now if we try to get value for key "Lukasz" we get null, because HashMap()<>; is already empty
+        System.out.println(nameToMeal.get("Lukasz"));
+
+        //when we want to display elements of map (keys and values)
+        System.out.println(nameToMeal.keySet());
+        Set<String> keys = nameToMeal.keySet();
+        for (String key : keys) {
+            System.out.println(key);
+        }
+
+        Collection<String> values = nameToMeal.values();
+        for (String value : values){
+            System.out.println(value);
+        }
+        //we can also check if map contains given key or value
+        System.out.println(nameToMeal.containsKey("Lukasz"));
+        System.out.println(nameToMeal.containsValue("Burger"));
     }
 }
